7a7022b21db6a176de2984bb0937fd32e0557440
Srishanth Devoju <srishanthdevoju5@gmail.com>
2025-09-10 15:15:49 +0530

    Update maxfreq.py

--- day-3/maxfreq.py
+++ day-3/maxfreq.py
@@ -1,11 +1,12 @@
 def string():
-    s=input()
-    dict={}
+    s = input()
+    count = {}
     for i in s:
-        if i in dict:
-            dict[i]+=1
+        if i in count:
+            count[i] += 1
         else:
-            dict[i]=1
-    for k in sorted(dict):
-        s=max(k,k=dict.values())
+            count[i] = 1
+    max_char = max(count, key=count.get)
+    print(max_char, count[max_char])
+
 string()
